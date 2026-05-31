047b10e5570f3d8bd8a063ebe56f617da74700d2
Javeria <javeria011@gmail.com>
2026-05-31 01:19:13 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        val sSorted = s.toCharArray().sorted()
+        val tSorted = t.toCharArray().sorted()
+        return sSorted == tSorted
+    }
+}
